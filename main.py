6c3124343919dd5e538c79376900a4b625dd1500
Jett Bradford <mrjettbradford@gmail.com>
2022-10-06 09:19:54 +0100

    Update output system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Pearson Pseudocode Transpiler
+import os
 import click
 
 from transpiler import Transpiler
@@ -10,9 +11,18 @@ from transpiler.tokenizer import Tokenizer
 
 @click.command()
 @click.argument('input_path')
-@click.argument('output_path')
+@click.argument('output_path', default="null")
 def transpile(input_path, output_path):
+    if not os.path.exists(input_path):
+        print("Failed to find the input file")
+        exit(1)
+
     input = open(input_path, "r")
+
+    if output_path == "null":
+        if not os.path.exists("output"): 
+            os.mkdir("output")
+        output_path = "output/" + os.path.basename(input_path) + ".py"
 
     tokenizer = Tokenizer()
     tokenizer.tokenize(input.read())
